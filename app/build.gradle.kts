a9217924b10430684f3525fea7a90692a9005459
mimikunda <tibohr.smohr@gmail.com>
2026-09-15 19:22:56 +0200

    ui: added partial mockup homescreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
@@ -51,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
--- app/src/main/java/com/mimikunda/gripgraf/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mimikunda/gripgraf/ui/screens/HomeScreen.kt
@@ -2,25 +2,53 @@
 
 package com.mimikunda.gripgraf.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.EmojiEvents
+import androidx.compose.material.icons.filled.EventAvailable
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Today
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExpandedFullScreenSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,12 +56,113 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     onNavigateToTable: () -> Unit,
     onNavigateToGraph: (Int) -> Unit
-){
-    Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = CenterHorizontally) {
-        MainSearchBar()
+) {
+
+    var currentDate by remember { mutableStateOf(LocalDate.now()) }
+    val currentDateString = remember(currentDate) {
+        currentDate.format(
+            DateTimeFormatter.ofPattern(
+                "EEEE, d. MMMM",
+                Locale.forLanguageTag("sl")
+            )
+        )
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = CenterHorizontally
+    ) {
+        item {
+            MainSearchBar()
+        }
+
+        item {
+            Spacer(Modifier.height(7.dp))
+            Text(
+                text = "Welcome, User!",
+                style = MaterialTheme.typography.displayMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = currentDateString,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            Spacer(Modifier.height(7.dp))
+        }
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                HighlightCard(
+                    modifier = Modifier.weight(1f),
+                    title = "Last PR",
+                    value = "105kg",
+                    icon = Icons.Default.EmojiEvents,
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+                HighlightCard(
+                    modifier = Modifier.weight(1f),
+                    title = "Today's attempt",
+                    value = "Incline Bench",
+                    icon = Icons.Default.Today,
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HighlightCard(
+    title: String,
+    value: String,
+    icon: ImageVector,
+    containerColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .height(130.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = containerColor
+        ),
+        shape = RoundedCornerShape(24.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(icon, contentDescription = null, modifier = Modifier.size(24.dp))
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    text = value,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 2,
+                    lineHeight = 22.sp
+                )
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium,
+                    maxLines = 1
+                )
+
+
+            }
+        }
 
     }
 }
+
 @Composable
 fun MainSearchBar() {
     val searchBarState = rememberSearchBarState()
