22bccca96d0ccfe519a05bee67eae3ed6b6c3a1b
mimikunda <tibohr.smohr@gmail.com>
2026-09-15 14:31:35 +0200

    chore: updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.mimikunda.gripgraf"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.mimikunda.gripgraf"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
